a5abe470581998909d77dcd2afa6e7369c9b586c
Taanveer22 <taanveer469@gmail.com>
2025-08-01 00:41:28 +0600

    alhamdulliah projects section added

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("connected");
+
+const video1 = document.getElementById("projectVideo1");
+const video2 = document.getElementById("projectVideo2");
+const video3 = document.getElementById("projectVideo3");
+const hoverSign = document.querySelector(".hover-sign");
+
+const videoList = [video1, video2, video3];
+console.log(videoList);
+videoList.forEach(function (video) {
+  video.addEventListener("mouseover", function () {
+    video.play();
+    hoverSign.classList.add("active");
+  });
+  video.addEventListener("mouseout", function () {
+    video.pause();
+    hoverSign.classList.remove("active");
+  });
+});
